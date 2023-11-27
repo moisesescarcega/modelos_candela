a254b18d3eb305fc79a28922a32bd4bda8315abf
Moises <moi.escarcega@pm.me>
2023-11-27 17:10:28 -0600

    otra prueba glb

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	ssr: { noExternal: ['three'] },
 	plugins: [sveltekit()],
-        server: {
-            fs: {
-                allow: ["/bolsa_mexicana_de_valores.glb"]
-            }
-        }
+        
     
 });
